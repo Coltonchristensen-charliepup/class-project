5371028ebe94daca6257063e8cb7297cb83ec8a4
coltonchristensen-charliepup <colton.james.christensen@gmail.com>
2020-09-15 19:13:26 -0700

    added content to file

--- images/add-content.js
+++ images/add-content.js
@@ -1,1 +1,8 @@
 document.write(' <h3>Welcome !</h3>');
+
+function getName(){
+    var userName = prompt('what is your name??');
+    return document.right('<h3>Hello ' + 'userName' + '!</h3>');
+}
+
+getName();
