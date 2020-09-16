05c9fddb6c284739784652de86107b9fa4a9d40f
coltonchristensen-charliepup <colton.james.christensen@gmail.com>
2020-09-16 16:10:38 -0700

    Add a loop that diplays a welcome message

--- images/add-content.js
+++ images/add-content.js
@@ -1,8 +1,15 @@
-document.write(' <h3>Welcome !</h3>');
+document.write('<h3>Welcome !</h3>');
 
 function getName(){
-    var userName = prompt('what is your name??');
-    return document.right('<h3>Hello ' + 'userName' + '!</h3>');
+    const userName = prompt('what is your name??');
+    // const value = `<h3>Hello  ${userName} ${userName}!</h3>`;
+    // document.write(value);
+    let aBunchOfUserNames = '';
+    for(let i = 0 ; i< 10; i++){
+        // aBunchOfUserNames = aBunchOfUserNames + userName
+        aBunchOfUserNames = `${aBunchOfUserNames} Hello ${userName} number ${i+1}</br>`;
+    }
+    document.write(`<h3>${aBunchOfUserNames}</h3>`)
 }
 
 getName();
