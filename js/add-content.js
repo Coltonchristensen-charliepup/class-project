0ac24fb4ad04a6a062c44de37eabe12e7be65dd2
coltonchristensen-charliepup <colton.james.christensen@gmail.com>
2020-09-16 20:29:01 -0700

    ask a question validate that question and response

--- js/add-content.js
+++ js/add-content.js
@@ -1,18 +1,35 @@
 document.write('<h3>Welcome !</h3>');
 
-function getName(){
-    let userName
-    while(!userName){
-        userName = prompt('what is your name??').trim()   
-    }
-    // const value = `<h3>Hello  ${userName} ${userName}!</h3>`;
-    // document.write(value);
-    let aBunchOfUserNames = '';
-    for(let i = 0 ; i< 10; i++){
-        // aBunchOfUserNames = aBunchOfUserNames + userName
-        aBunchOfUserNames = `${aBunchOfUserNames} Hello ${userName} number ${i+1}</br>`;
-    }
-    document.write(`<h3>${aBunchOfUserNames}</h3>`)
+function ensureIsNotBlank(userInput) {
+    return !!userInput && userInput.length >1 
 }
 
+function ensureIsNumber(userInput) {
+    // return true if the value is good
+    // return false if value is bad
+    const isNotANumber = isNaN(userInput)
+    return !isNotANumber
+}
+
+function getName() {
+    const userName = getUserInput('what is your name??', ensureIsNotBlank)
+    const quanityOfBiscuits = getUserInput('how many dog biscuits do you want to give charlie??', ensureIsNumber)
+
+    const value = `<h3>Hello  ${userName}!</h3>`;
+    document.write(value);
+    let charliesbiscuits = '';
+    for(let i = 0 ; i< quanityOfBiscuits; i++){
+        charliesbiscuits = `${charliesbiscuits} we fed charlie biscuit ${i+1}</br>`;
+    }
+    document.write(`<h3>${charliesbiscuits}</h3>`)
+}
+
+function getUserInput(question, validator) {
+    let userInput
+    while (!validator(userInput)) {
+        userInput = prompt(question).trim()
+    }
+    return userInput
+}
 getName();
+
